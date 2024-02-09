9508409de01107c2e3128bced85e35dc31102d2d
Goinesjalen <goinesjalen@gmail.com>
2024-02-08 22:32:37 -0500

    practicum 4 p2

--- src/index.js
+++ src/index.js
@@ -19,4 +19,16 @@ app.get('/add', (req, res) => {
     }
 });
 
+app.get('/subtract', (req, res) => {
+    const num1 = parseFloat(req.query.num1);
+    const num2 = parseFloat(req.query.num2);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        res.status(400).send('Invalid numbers provided');
+    } else {
+        const result = num1 - num2;
+        res.json({ result: result });
+    }
+});
+
 app.listen(port);
